bd933e128187d12f22039a0466bfca6bbc0ea352
Raj Soren <7rajkumarsoren@gmail.com>
2025-01-25 22:44:54 +0530

    fix no-unused-vars

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-unused-vars */
 import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className='relative'>
+      <Nav />
+      <section className='xl: padding-l wide: padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Services/>
+      </section>
+      <section className='padding'>
+        <SpecialOffers/>
+      </section>
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews/>
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className='bg-black padding-x padding-t'>
+        <Footer/>
+      </section>
+    </main>
   )
 }
 
